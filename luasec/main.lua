9baf6ea7803e9ab247d1b7907840577e90e50f82
Perry Clarke <perry@coronalabs.com>
2016-05-19 11:34:02 -0700

    Updated samples

--- luasec/main.lua
+++ luasec/main.lua
@@ -1,41 +1,47 @@
 --------------------------------------------------------------------------------
--- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
--- Copyright (C) 2013 Corona Labs Inc. All Rights Reserved.
+-- Sample code is MIT licensed, see https://coronalabs.com/links/code/license
+-- Copyright (C) 2016 Corona Labs Inc. All Rights Reserved.
 --------------------------------------------------------------------------------
---
--- To test this client-side code, use this server:
---
---		openssl s_server \
---		-key path/to/my/private_key.pem \
---		-cert path/to/my/signed_public_key_certificate.pem \
---		-accept 64001 -www
 
-print( "lua-openssl secure socket test start." )
+--
+-- Demonstrates two ways to retrieve data from secure web servers 
+--
 
+local greeting = display.newText("luasec tests - see console for results", display.actualContentWidth/2, 20)
+
+print( "plugin.openssl secure socket test start" )
+
+local json = require('json')
 local openssl = require('plugin.openssl')
 local socket = require('socket')
+local http = require('socket.http')
+local ltn12 = require('ltn12')
 local plugin_luasec_ssl = require('plugin_luasec_ssl')
 
 lua_openssl_version, lua_version, openssl_version = openssl.version()
 print( "lua-openssl version: " .. lua_openssl_version, lua_version, openssl_version )
 
 --------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
--- TLS/SSL client parameters (omitted)
+local serverName = "coronalabs.com"
+
+print("Requesting secure web page on raw TCP socket from: " .. serverName)
+
+-- TLS/SSL client parameters
 local params =
 {
 	mode = "client",
-	protocol = "tlsv1",
+	protocol = "tlsv1_2", -- "any"
 	verify = "none",
 	options = "all",
 }
 
 local conn = socket.tcp()
+local server_address = socket.dns.toip(serverName)
+local server_port = 443
 
-local server_address = "10.3.3.106"
-local server_port = 64001
-
+print("    Connecting to: " .. string.format("%s:%s", server_address, server_port))
+conn:settimeout(10)
 local result, error = conn:connect( server_address, server_port )
 if result then
 	-- We're connected.
@@ -45,22 +51,58 @@ else
 end
 
 -- TLS/SSL initialization
-conn = plugin_luasec_ssl.wrap(conn, params)
-conn:dohandshake()
---
-conn:send( "GET / HTTP/1.0\n\n" )
+conn, msg = plugin_luasec_ssl.wrap(conn, params)
+
+if not conn then
+	print("wrap failed: ", msg)
+end
+
+local result, msg = conn:dohandshake()
+if not result then
+	print("conn:dohandshake: failed with:", msg)
+end
+
+local conn_info = conn:info()
+if conn_info then
+	local fmtedstr, _ = json.prettify(conn_info):gsub("\n", "\n    ")
+	print("    conn:info: " .. fmtedstr)
+end
+
+conn:send( "GET / HTTP/1.0\nHost: coronalabs.com\n\n" )
 
 local data, status, partial_data = conn:receive("*a")
-if data then
-	print( data )
-end
+
 if partial_data then
-	print( partial_data )
+	data = data.partial_data
 end
+
+print("    received ".. data:len() .." bytes (raw response is larger due to HTTP headers)")
 
 conn:close()
 
---------------------------------------------------------------------------------
+print("")
+
 --------------------------------------------------------------------------------
 
-print( "lua-openssl secure socket test done." )
+local plugin_luasec_https = require('plugin_luasec_https')
+local lfs = require('lfs')
+
+local url = "https://coronalabs.com/"
+local outfile = system.pathForFile('out.dat', system.TemporaryDirectory)
+local outfilePtr = io.open(outfile, 'w')
+
+print("Requesting secure URL on HTTPS socket: " .. url)
+
+plugin_luasec_https.request({
+				url = "https://coronalabs.com/",
+				sink = ltn12.sink.file(outfilePtr),
+				protocol = "any"
+			})
+
+print("    received ".. lfs.attributes(outfile).size .." bytes")
+
+--------------------------------------------------------------------------------
+
+print("")
+
+print( "plugin.openssl secure socket samples done" )
